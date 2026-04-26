31c28f806f33d9183702005c977e6980f933944a
Abhishek Dasgupta <abhishek.dasgupta11@gmail.com>
2026-04-26 11:31:12 +0530

    Update devTest.groovy

--- vars/devTest.groovy
+++ vars/devTest.groovy
@@ -2,7 +2,7 @@ def checkout() {
     echo "Hello, This is ${name} from devops team"
 }
 def build() {
-    echo "Hello, This is ${name} from devops team"
+    echo "Hello, This is Abhishek from devops team"
     sh 'docker build -t nginx-jenkins .'
 }
 def deploy() {

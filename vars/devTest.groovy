4d074d8906f5f4c673d7e4bce6c5239795a408c3
Abhishek Dasgupta <abhishek.dasgupta11@gmail.com>
2026-04-26 11:52:47 +0530

    Update devTest.groovy

--- vars/devTest.groovy
+++ vars/devTest.groovy
@@ -2,9 +2,15 @@ def checkout() {
     echo "Hello, This is ${name} from devops team"
 }
 def build() {
-    echo "Hello, This is Abhishek from devops team"
+    echo "Hello, This is for Building the Docker-Image"
     sh 'docker build -t nginx-jenkins .'
 }
 def deploy() {
-    echo "Hello, This is ${name} from devops team"
+    echo "Hello, This is for Tagging and Pushing the Docker-Image into DOCKER-HUB"
+    sh '''
+        echo $DOCKER_LOGIN_PSW | docker login -u $DOCKER_LOGIN_USR --password-stdin
+        '''
+        sh 'docker --version'
+        sh 'docker tag nginx-jenkins:latest adasgupt86/nginx-jenkins:v1'
+        sh 'docker push adasgupt86/nginx-jenkins:v1'
 }

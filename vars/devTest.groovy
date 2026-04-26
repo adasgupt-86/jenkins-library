ed5d43481dca1094a715e610615f6e6aacbb9f4a
root <root@localhost.localdomain>
2026-04-26 11:10:05 +0530

    adding library fuctions

---
+++ vars/devTest.groovy
@@ -0,0 +1,9 @@
+def checkout() {
+    echo "Hello, This is ${name} from devops team"
+}
+def build() {
+    echo "Hello, This is ${name} from devops team"
+}
+def deploy() {
+    echo "Hello, This is ${name} from devops team"
+}
